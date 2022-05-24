c067828cc88012c64f056a201efbef85073ec005
Hunter <huntercbaldwin98@gmail.com>
2022-05-24 14:46:43 -0700

    feature: rocket boost sound effect

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Header("Movement")] float rotationSpeed = 10;
 
     Rigidbody _rigidBody;
+    [SerializeField, Header("Audio")] AudioSource audioSource;
     float _verticalMovement = 0f;
     float _rotationalMovement;
     
@@ -19,11 +20,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         ProcessInput();
+
     }
 
     void FixedUpdate()
     {
         Fly();
+        PlayRocketSound();
     }
     
     void ProcessInput()
@@ -34,12 +37,30 @@ public class PlayerController : MonoBehaviour
 
     void Fly()
     {
-        
         _rigidBody.freezeRotation = true;
         Vector3 rotation = new Vector3(0, 0, rotationSpeed * -_rotationalMovement * Time.fixedDeltaTime);
         transform.Rotate(rotation);
         _rigidBody.freezeRotation = false;
 
         _rigidBody.AddRelativeForce(Vector3.up * (thrustSpeed * _verticalMovement * Time.fixedDeltaTime));
+    }
+
+
+    void PlayRocketSound()
+    {
+        if ((!audioSource.isPlaying || audioSource.volume < 0.1f) && _verticalMovement > 0) 
+        {
+            audioSource.volume = 0.1f;
+            audioSource.Play();
+        }
+        
+        if (_verticalMovement == 0)
+        {
+            audioSource.volume -= 0.0025f;
+            if (audioSource.volume == 0)
+            {
+                audioSource.Stop();
+            }
+        }
     }
 }
